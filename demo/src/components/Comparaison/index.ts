35f1379877c6e51106306a1f841978f6ad239515
Adrien Rosi <adrienrosi@gmail.com>
2025-05-30 14:50:41 +0200

    add Comparaison

---
+++ demo/src/components/Comparaison/index.ts
@@ -0,0 +1,3 @@
+export { default as Bilan } from './Bilan.astro';
+export { default as ConsommationsEnergie } from './ConsommationsEnergie.astro';
+export { default as ConsommationsUsage } from './ConsommationsUsage.astro';
---
+++ src/components/Comparaison/index.ts
@@ -0,0 +1,2 @@
+export { default as Comparaison } from './Comparaison.astro'
+export { default as ComparaisonItem } from './ComparaisonItem.astro'
